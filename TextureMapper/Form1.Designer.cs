8785e1f771ec83f762678425a4459adeb21dbcca
User255 <User255@DESKTOP-297A92S>
2024-03-30 16:47:38 -0300

    Adicionar arquivos de projeto.

---
+++ TextureMapper/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace TextureMapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblXScale = new System.Windows.Forms.Label();
+            this.numXScale = new System.Windows.Forms.NumericUpDown();
+            this.numYScale = new System.Windows.Forms.NumericUpDown();
+            this.lblYScale = new System.Windows.Forms.Label();
+            this.numYPosition = new System.Windows.Forms.NumericUpDown();
+            this.lblYPosition = new System.Windows.Forms.Label();
+            this.numXPosition = new System.Windows.Forms.NumericUpDown();
+            this.lblXPosition = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numXScale)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numYScale)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numYPosition)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numXPosition)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(860, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // lblXScale
+            // 
+            this.lblXScale.AutoSize = true;
+            this.lblXScale.Location = new System.Drawing.Point(530, 27);
+            this.lblXScale.Name = "lblXScale";
+            this.lblXScale.Size = new System.Drawing.Size(47, 13);
+            this.lblXScale.TabIndex = 2;
+            this.lblXScale.Text = "X Scale:";
+            this.lblXScale.Visible = false;
+            // 
+            // numXScale
+            // 
+            this.numXScale.Location = new System.Drawing.Point(593, 25);
+            this.numXScale.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.numXScale.Name = "numXScale";
+            this.numXScale.Size = new System.Drawing.Size(86, 20);
+            this.numXScale.TabIndex = 3;
+            this.numXScale.Visible = false;
+            // 
+            // numYScale
+            // 
+            this.numYScale.Location = new System.Drawing.Point(766, 25);
+            this.numYScale.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.numYScale.Name = "numYScale";
+            this.numYScale.Size = new System.Drawing.Size(86, 20);
+            this.numYScale.TabIndex = 5;
+            this.numYScale.Visible = false;
+            // 
+            // lblYScale
+            // 
+            this.lblYScale.AutoSize = true;
+            this.lblYScale.Location = new System.Drawing.Point(703, 27);
+            this.lblYScale.Name = "lblYScale";
+            this.lblYScale.Size = new System.Drawing.Size(47, 13);
+            this.lblYScale.TabIndex = 4;
+            this.lblYScale.Text = "Y Scale:";
+            this.lblYScale.Visible = false;
+            // 
+            // numYPosition
+            // 
+            this.numYPosition.Location = new System.Drawing.Point(766, 62);
+            this.numYPosition.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.numYPosition.Name = "numYPosition";
+            this.numYPosition.Size = new System.Drawing.Size(86, 20);
+            this.numYPosition.TabIndex = 9;
+            this.numYPosition.Visible = false;
+            // 
+            // lblYPosition
+            // 
+            this.lblYPosition.AutoSize = true;
+            this.lblYPosition.Location = new System.Drawing.Point(703, 64);
+            this.lblYPosition.Name = "lblYPosition";
+            this.lblYPosition.Size = new System.Drawing.Size(57, 13);
+            this.lblYPosition.TabIndex = 8;
+            this.lblYPosition.Text = "Y Position:";
+            this.lblYPosition.Visible = false;
+            // 
+            // numXPosition
+            // 
+            this.numXPosition.Location = new System.Drawing.Point(593, 62);
+            this.numXPosition.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+            this.numXPosition.Name = "numXPosition";
+            this.numXPosition.Size = new System.Drawing.Size(86, 20);
+            this.numXPosition.TabIndex = 7;
+            this.numXPosition.Visible = false;
+            // 
+            // lblXPosition
+            // 
+            this.lblXPosition.AutoSize = true;
+            this.lblXPosition.Location = new System.Drawing.Point(530, 64);
+            this.lblXPosition.Name = "lblXPosition";
+            this.lblXPosition.Size = new System.Drawing.Size(57, 13);
+            this.lblXPosition.TabIndex = 6;
+            this.lblXPosition.Text = "X Position:";
+            this.lblXPosition.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(731, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 545);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numYPosition);
+            this.Controls.Add(this.lblYPosition);
+            this.Controls.Add(this.numXPosition);
+            this.Controls.Add(this.lblXPosition);
+            this.Controls.Add(this.numYScale);
+            this.Controls.Add(this.lblYScale);
+            this.Controls.Add(this.numXScale);
+            this.Controls.Add(this.lblXScale);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "GenericTextureMapper";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numXScale)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numYScale)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numYPosition)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numXPosition)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Label lblXScale;
+        private System.Windows.Forms.NumericUpDown numXScale;
+        private System.Windows.Forms.NumericUpDown numYScale;
+        private System.Windows.Forms.Label lblYScale;
+        private System.Windows.Forms.NumericUpDown numYPosition;
+        private System.Windows.Forms.Label lblYPosition;
+        private System.Windows.Forms.NumericUpDown numXPosition;
+        private System.Windows.Forms.Label lblXPosition;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ TextureMapper/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextureMapper
+{
+    public partial class Form1 : Form
+    {
+        string binaryPath;
+        string texturePath;
+        int XScale;
+        int YScale;
+        int XPosition;
+        int YPosition;
+
+        Bitmap originalImage;
+
+        private bool isDragging = false;
+        private Point lastLocation;
+
+        private bool isResizing = false;
+        private Point resizingStartPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            numXPosition.ValueChanged += NumXPosition_ValueChanged;
+            numYPosition.ValueChanged += NumYPosition_ValueChanged;
+            numXScale.ValueChanged += NumXScale_ValueChanged;
+            numYScale.ValueChanged += NumYScale_ValueChanged;
+
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+
+            this.pictureBox1.Paint += new PaintEventHandler(this.pictureBox1_Paint);
+
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (new Rectangle(XPosition, YPosition, XScale, YScale).Contains(e.Location))
+                {
+                    Rectangle resizingRect = new Rectangle(XPosition + XScale - 5, YPosition + YScale - 5, 10, 10);
+                    if (resizingRect.Contains(e.Location))
+                    {
+                        isResizing = true;
+                        resizingStartPoint = e.Location;
+                    }
+                    else
+                    {
+                        isDragging = true;
+                        lastLocation = e.Location;
+                    }
+                }
+            }
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                int deltaX = e.X - lastLocation.X;
+                int deltaY = e.Y - lastLocation.Y;
+
+                int newX = XPosition + deltaX;
+                int newY = YPosition + deltaY;
+
+                if (newX >= 0 && newX + XScale <= pictureBox1.Width &&
+                    newY >= 0 && newY + YScale <= pictureBox1.Height)
+                {
+                    XPosition = newX;
+                    YPosition = newY;
+
+                    lastLocation = e.Location;
+
+                    pictureBox1.Invalidate();
+
+                    UpdateNumericUpDownValues();
+
+                    FUpdateNumericUpDownValues();
+                }
+            }
+            else if (isResizing)
+            {
+                int deltaX = e.X - resizingStartPoint.X;
+                int deltaY = e.Y - resizingStartPoint.Y;
+
+                int newWidth = XScale + deltaX;
+                int newHeight = YScale + deltaY;
+
+                if (newWidth > 0 && newHeight > 0 &&
+                    XPosition + newWidth <= pictureBox1.Width &&
+                    YPosition + newHeight <= pictureBox1.Height)
+                {
+                    XScale = newWidth;
+                    YScale = newHeight;
+
+                    resizingStartPoint = e.Location;
+
+                    pictureBox1.Invalidate();
+
+                    UpdateNumericUpDownValues();
+
+                    FUpdateNumericUpDownValues();
+                }
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+            isResizing = false;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(128, Color.Orange)), XPosition, YPosition, XScale, YScale);
+
+            e.Graphics.FillRectangle(Brushes.Blue, XPosition + XScale - 5, YPosition + YScale - 5, 10, 10);
+        }
+
+        private void FUpdateNumericUpDownValues()
+        {
+            numXPosition.Value = XPosition;
+            numYPosition.Value = YPosition;
+            numXScale.Value = XScale;
+            numYScale.Value = YScale;
+        }
+
+        private void UpdateNumericUpDownValues()
+        {
+            numXPosition.ValueChanged -= NumXPosition_ValueChanged;
+            numYPosition.ValueChanged -= NumYPosition_ValueChanged;
+            numXScale.ValueChanged -= NumXScale_ValueChanged;
+            numYScale.ValueChanged -= NumYScale_ValueChanged;
+
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                int selectedIndex = comboBox1.SelectedIndex;
+                numXPosition.Value = XPosition;
+                numYPosition.Value = YPosition;
+                numXScale.Value = XScale;
+                numYScale.Value = YScale;
+
+                XPositions[selectedIndex] = (int)numXPosition.Value;
+                YPositions[selectedIndex] = (int)numYPosition.Value;
+                XScales[selectedIndex] = (int)numXScale.Value;
+                YScales[selectedIndex] = (int)numYScale.Value;
+            }
+
+            numXPosition.ValueChanged += NumXPosition_ValueChanged;
+            numYPosition.ValueChanged += NumYPosition_ValueChanged;
+            numXScale.ValueChanged += NumXScale_ValueChanged;
+            numYScale.ValueChanged += NumYScale_ValueChanged;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Title = "Select the Binary File",
+                Filter = "Binary File|*.bin|All Files|*.*"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                binaryPath = openFileDialog.FileName;
+
+                openFileDialog = new OpenFileDialog()
+                {
+                    Title = "Select the Texture File",
+                    Filter = "Texture file|*.png|All Files|*.*"
+                };
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    texturePath = openFileDialog.FileName;
+
+                    ReadBinaryFile();
+                }
+            }
+        }
+
+        private int[] XPositions;
+        private int[] YPositions;
+        private int[] XScales;
+        private int[] YScales;
+
+        private void ReadBinaryFile()
+        {
+            comboBox1.Items.Clear();
+
+            using (FileStream fs = new FileStream(binaryPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                int counter = 0;
+                byte[] buffer = new byte[8];
+
+                int fileSize = (int)(fs.Length / buffer.Length);
+                XPositions = new int[fileSize];
+                YPositions = new int[fileSize];
+                XScales = new int[fileSize];
+                YScales = new int[fileSize];
+
+                while (fs.Read(buffer, 0, buffer.Length) > 0)
+                {
+                    short xPosition = BitConverter.ToInt16(buffer, 0);
+                    short yPosition = BitConverter.ToInt16(buffer, 2);
+                    short xScale = BitConverter.ToInt16(buffer, 4);
+                    short yScale = BitConverter.ToInt16(buffer, 6);
+
+                    XPositions[counter] = xPosition;
+                    YPositions[counter] = yPosition;
+                    XScales[counter] = xScale;
+                    YScales[counter] = yScale;
+
+                    comboBox1.Items.Add($"Item {counter}");
+
+                    if (counter == 0)
+                    {
+                        XPosition = xPosition;
+                        YPosition = yPosition;
+                        XScale = xScale;
+                        YScale = yScale;
+
+                        UpdateNumericUpDownValues();
+
+                        XPosition = xPosition;
+                        YPosition = yPosition;
+                        XScale = xScale;
+                        YScale = yScale;
+
+                        originalImage = (Bitmap)Bitmap.FromFile(texturePath);
+
+                        int imageWidth = originalImage.Width;
+                        int imageHeight = originalImage.Height;
+
+                        numXPosition.Maximum = imageWidth;
+                        numYPosition.Maximum = imageHeight;
+                        numXScale.Maximum = imageWidth;
+                        numYScale.Maximum = imageHeight;
+
+                        Rectangle cropRect = new Rectangle(0, 0, imageWidth, imageHeight);
+                        Bitmap croppedImage = originalImage.Clone(cropRect, originalImage.PixelFormat);
+                        pictureBox1.Image = croppedImage;
+
+                        originalImage = (Bitmap)Bitmap.FromFile(texturePath);
+
+                        pictureBox1.Invalidate();
+                    }
+
+                    counter++;
+                }
+
+                comboBox1.SelectedIndex = 0;
+                pictureBox1.Visible = true;
+                numXPosition.Visible = true;
+                lblXPosition.Visible = true;
+                numYPosition.Visible = true;
+                lblYPosition.Visible = true;
+                numXScale.Visible = true;
+                lblXScale.Visible = true;
+                numYScale.Visible = true;
+                lblYScale.Visible = true;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = comboBox1.SelectedIndex;
+
+            if (selectedIndex >= 0 && selectedIndex < XPositions.Length)
+            {
+                int xPosition = XPositions[selectedIndex];
+                int yPosition = YPositions[selectedIndex];
+                int xScale = XScales[selectedIndex];
+                int yScale = YScales[selectedIndex];
+
+                XPosition = xPosition;
+                YPosition = yPosition;
+                XScale = xScale;
+                YScale = yScale;
+
+                pictureBox1.Invalidate();
+
+                UpdateNumericUpDownValues();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Color corComOpacidade = Color.FromArgb(128, Color.Orange);
+
+            SolidBrush brush = new SolidBrush(corComOpacidade);
+
+            int x = XPosition;
+            int y = YPosition;
+            int largura = XScale;
+            int altura = YScale;
+
+            g.FillRectangle(brush, x, y, largura, altura);
+        }
+
+        private void NumXPosition_ValueChanged(object sender, EventArgs e)
+        {
+            XPosition = (int)numXPosition.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void NumYPosition_ValueChanged(object sender, EventArgs e)
+        {
+            YPosition = (int)numYPosition.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void NumXScale_ValueChanged(object sender, EventArgs e)
+        {
+            XScale = (int)numXScale.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void NumYScale_ValueChanged(object sender, EventArgs e)
+        {
+            YScale = (int)numYScale.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "Binary File (*.bin)|*.bin|Todos os arquivos (*.*)|*.*";
+            saveFileDialog.Title = "Save Binary File";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+                {
+                    for (int i = 0; i < XPositions.Length; i++)
+                    {
+                        short xPosition = (short)Math.Min(XPositions[i], 65535);
+                        short yPosition = (short)Math.Min(YPositions[i], 65535);
+                        short xScale = (short)Math.Min(XScales[i], 65535);
+                        short yScale = (short)Math.Min(YScales[i], 65535);
+
+                        writer.Write(xPosition);
+                        writer.Write(yPosition);
+                        writer.Write(xScale);
+                        writer.Write(yScale);
+                    }
+                }
+
+                MessageBox.Show("File saved successfully!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A very lazy tool made by zMath3usMSF that maps texture based on clipping.\n\nOrder of data in the binary file must be X Position > Y Position > Width and Height, all in Int16.");
+        }
+    }
+}
